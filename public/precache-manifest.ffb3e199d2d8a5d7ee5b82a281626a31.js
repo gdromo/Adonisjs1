04645690fccb9ea8ec8e0840c19ae829707ebc93
GuillermoR <gdromo@hotmail.com>
2019-08-16 21:26:33 -0300

    v7

---
+++ public/precache-manifest.ffb3e199d2d8a5d7ee5b82a281626a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1b7994b2c4ba90cd67d706fb65538e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04938ae7f223fe10d85e",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "ae6691e6696e97d4b89c",
+    "url": "/static/css/main.1cf1ecff.chunk.css"
+  },
+  {
+    "revision": "04938ae7f223fe10d85e",
+    "url": "/static/js/2.4f69c9fc.chunk.js"
+  },
+  {
+    "revision": "ae6691e6696e97d4b89c",
+    "url": "/static/js/main.026e94ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
